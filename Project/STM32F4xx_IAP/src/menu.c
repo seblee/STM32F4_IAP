e95ae38d5c4999d3f5bc5a9785c55eaed602e1cc
Seblee <ceeu@vip.qq.com>
2019-03-07 15:50:15 +0800

    change System Clock source PLL (HSI)

--- Project/STM32F4xx_IAP/src/menu.c
+++ Project/STM32F4xx_IAP/src/menu.c
@@ -23,7 +23,7 @@
   *
   * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
   ******************************************************************************
-  */ 
+  */
 
 /** @addtogroup STM32F4xx_IAP
   * @{
@@ -42,10 +42,7 @@
 pFunction Jump_To_Application;
 uint32_t JumpAddress;
 __IO uint32_t FlashProtection = 0;
-uint8_t tab_1024[1024] =
-  {
-    0
-  };
+uint8_t tab_1024[1024] = {0};
 uint8_t FileName[FILE_NAME_LENGTH];
 
 /* Private function prototypes -----------------------------------------------*/
@@ -61,37 +58,37 @@ void SerialUpload(void);
   */
 void SerialDownload(void)
 {
-  uint8_t Number[10] = "          ";
-  int32_t Size = 0;
+    uint8_t Number[10] = "          ";
+    int32_t Size = 0;
 
-  SerialPutString("Waiting for the file to be sent ... (press 'a' to abort)\n\r");
-  Size = Ymodem_Receive(&tab_1024[0]);
-  if (Size > 0)
-  {
-    SerialPutString("\n\n\r Programming Completed Successfully!\n\r--------------------------------\r\n Name: ");
-    SerialPutString(FileName);
-    Int2Str(Number, Size);
-    SerialPutString("\n\r Size: ");
-    SerialPutString(Number);
-    SerialPutString(" Bytes\r\n");
-    SerialPutString("-------------------\n");
-  }
-  else if (Size == -1)
-  {
-    SerialPutString("\n\n\rThe image size is higher than the allowed space memory!\n\r");
-  }
-  else if (Size == -2)
-  {
-    SerialPutString("\n\n\rVerification failed!\n\r");
-  }
-  else if (Size == -3)
-  {
-    SerialPutString("\r\n\nAborted by user.\n\r");
-  }
-  else
-  {
-    SerialPutString("\n\rFailed to receive the file!\n\r");
-  }
+    SerialPutString("Waiting for the file to be sent ... (press 'a' to abort)\n\r");
+    Size = Ymodem_Receive(&tab_1024[0]);
+    if (Size > 0)
+    {
+        SerialPutString("\n\n\r Programming Completed Successfully!\n\r--------------------------------\r\n Name: ");
+        SerialPutString(FileName);
+        Int2Str(Number, Size);
+        SerialPutString("\n\r Size: ");
+        SerialPutString(Number);
+        SerialPutString(" Bytes\r\n");
+        SerialPutString("-------------------\n");
+    }
+    else if (Size == -1)
+    {
+        SerialPutString("\n\n\rThe image size is higher than the allowed space memory!\n\r");
+    }
+    else if (Size == -2)
+    {
+        SerialPutString("\n\n\rVerification failed!\n\r");
+    }
+    else if (Size == -3)
+    {
+        SerialPutString("\r\n\nAborted by user.\n\r");
+    }
+    else
+    {
+        SerialPutString("\n\rFailed to receive the file!\n\r");
+    }
 }
 
 /**
@@ -101,24 +98,26 @@ void SerialDownload(void)
   */
 void SerialUpload(void)
 {
-  uint8_t status = 0 ; 
+    uint8_t status = 0;
 
-  SerialPutString("\n\n\rSelect Receive File\n\r");
+    SerialPutString("\n\n\rSelect Receive File\n\r");
 
-  if (GetKey() == CRC16)
-  {
-    /* Transmit the flash image through ymodem protocol */
-    status = Ymodem_Transmit((uint8_t*)APPLICATION_ADDRESS, (const uint8_t*)"UploadedFlashImage.bin", USER_FLASH_SIZE);
-
-    if (status != 0) 
+    if (GetKey() == CRC16)
     {
-      SerialPutString("\n\rError Occurred while Transmitting File\n\r");
+        /* Transmit the flash image through ymodem protocol */
+        status = Ymodem_Transmit((uint8_t *)APPLICATION_ADDRESS, (const uint8_t *)"UploadedFlashImage.bin", USER_FLASH_SIZE);
+
+        if (status != 0)
+            if (status != 0)
+                if (status != 0)
+                {
+                    SerialPutString("\n\rError Occurred while Transmitting File\n\r");
+                }
+                else
+                {
+                    SerialPutString("\n\rFile uploaded successfully \n\r");
+                }
     }
-    else
-    {
-      SerialPutString("\n\rFile uploaded successfully \n\r");
-    }
-  }
 }
 
 /**
@@ -128,96 +127,136 @@ void SerialUpload(void)
   */
 void Main_Menu(void)
 {
-  uint8_t key = 0;
+    uint8_t key = 0;
+    uint8_t strtemp[12];
+    RCC_ClocksTypeDef RCC_Clocks;
+    for (key = 0; key < 12; key++)
+        strtemp[key] = 0;
+    SystemCoreClockUpdate();
+    Int2Str(strtemp, SystemCoreClock);
+    SerialPutString("            SystemCoreClock:");
+    SerialPutString(strtemp);
+    SerialPutString("\r\n");
 
-  SerialPutString("\r\n======================================================================");
-  SerialPutString("\r\n=              (C) COPYRIGHT 2011 STMicroelectronics                 =");
-  SerialPutString("\r\n=                                                                    =");
-  SerialPutString("\r\n=  STM32F4xx In-Application Programming Application  (Version 1.0.0) =");
-  SerialPutString("\r\n=                                                                    =");
-  SerialPutString("\r\n=                                   By MCD Application Team          =");
-  SerialPutString("\r\n======================================================================");
-  SerialPutString("\r\n\r\n");
+    for (key = 0; key < 12; key++)
+        strtemp[key] = 0;
+    RCC_GetClocksFreq(&RCC_Clocks);
+    Int2Str(strtemp, RCC_Clocks.SYSCLK_Frequency);
+    SerialPutString("RCC_Clocks.SYSCLK_Frequency:");
+    SerialPutString(strtemp);
+    SerialPutString("\r\n");
 
-  /* Test if any sector of Flash memory where user application will be loaded is write protected */
-  if (FLASH_If_GetWriteProtectionStatus() == 0)   
-  {
-    FlashProtection = 1;
-  }
-  else
-  {
-    FlashProtection = 0;
-  }
+    for (key = 0; key < 12; key++)
+        strtemp[key] = 0;
+    Int2Str(strtemp, RCC_Clocks.HCLK_Frequency);
+    SerialPutString("  RCC_Clocks.HCLK_Frequency:");
+    SerialPutString(strtemp);
+    SerialPutString("\r\n");
 
-  while (1)
-  {
-    SerialPutString("\r\n================== Main Menu ============================\r\n\n");
-    SerialPutString("  Download Image To the STM32F4xx Internal Flash ------- 1\r\n\n");
-    SerialPutString("  Upload Image From the STM32F4xx Internal Flash ------- 2\r\n\n");
-    SerialPutString("  Execute The New Program ------------------------------ 3\r\n\n");
+    for (key = 0; key < 12; key++)
+        strtemp[key] = 0;
+    Int2Str(strtemp, RCC_Clocks.PCLK1_Frequency);
+    SerialPutString(" RCC_Clocks.PCLK1_Frequency:");
+    SerialPutString(strtemp);
+    SerialPutString("\r\n");
 
-    if(FlashProtection != 0)
+    for (key = 0; key < 12; key++)
+        strtemp[key] = 0;
+    Int2Str(strtemp, RCC_Clocks.PCLK2_Frequency);
+    SerialPutString(" RCC_Clocks.PCLK2_Frequency:");
+    SerialPutString(strtemp);
+    SerialPutString("\r\n");
+
+    SerialPutString("\r\n======================================================================");
+    SerialPutString("\r\n=              (C) COPYRIGHT 2011 STMicroelectronics                 =");
+    SerialPutString("\r\n=                                                                    =");
+    SerialPutString("\r\n=  STM32F4xx In-Application Programming Application  (Version 1.0.0) =");
+    SerialPutString("\r\n=                                                                    =");
+    SerialPutString("\r\n=                                   By MCD Application Team          =");
+    SerialPutString("\r\n======================================================================");
+    SerialPutString("\r\n\r\n");
+
+    /* Test if any sector of Flash memory where user application will be loaded is write protected */
+    if (FLASH_If_GetWriteProtectionStatus() == 0)
+        if (FLASH_If_GetWriteProtectionStatus() == 0)
+            if (FLASH_If_GetWriteProtectionStatus() == 0)
+            {
+                FlashProtection = 1;
+            }
+            else
+            {
+                FlashProtection = 0;
+            }
+
+    while (1)
     {
-      SerialPutString("  Disable the write protection ------------------------- 4\r\n\n");
-    }
+        SerialPutString("\r\n================== Main Menu ============================\r\n\n");
+        SerialPutString("  Download Image To the STM32F4xx Internal Flash ------- 1\r\n\n");
+        SerialPutString("  Upload Image From the STM32F4xx Internal Flash ------- 2\r\n\n");
+        SerialPutString("  Execute The New Program ------------------------------ 3\r\n\n");
 
-    SerialPutString("==========================================================\r\n\n");
-
-    /* Receive key */
-    key = GetKey();
-
-    if (key == 0x31)
-    {
-      /* Download user application in the Flash */
-      SerialDownload();
-    }
-    else if (key == 0x32)
-    {
-      /* Upload user application from the Flash */
-      SerialUpload();
-    }
-    else if (key == 0x33) /* execute the new program */
-    {
-      JumpAddress = *(__IO uint32_t*) (APPLICATION_ADDRESS + 4);
-      /* Jump to user application */
-      Jump_To_Application = (pFunction) JumpAddress;
-      /* Initialize user application's Stack Pointer */
-      __set_MSP(*(__IO uint32_t*) APPLICATION_ADDRESS);
-      Jump_To_Application();
-    }
-    else if ((key == 0x34) && (FlashProtection == 1))
-    {
-      /* Disable the write protection */
-      switch (FLASH_If_DisableWriteProtection())
-      {
-        case 1:
+        if (FlashProtection != 0)
         {
-          SerialPutString("Write Protection disabled...\r\n");
-          FlashProtection = 0;
-          break;
+            SerialPutString("  Disable the write protection ------------------------- 4\r\n\n");
         }
-        case 2:
+
+        SerialPutString("==========================================================\r\n\n");
+
+        /* Receive key */
+        key = GetKey();
+
+        if (key == 0x31)
         {
-          SerialPutString("Error: Flash write unprotection failed...\r\n");
-          break;
+            /* Download user application in the Flash */
+            SerialDownload();
         }
-        default:
+        else if (key == 0x32)
         {
+            /* Upload user application from the Flash */
+            SerialUpload();
         }
-      }
+        else if (key == 0x33) /* execute the new program */
+        {
+            JumpAddress = *(__IO uint32_t *)(APPLICATION_ADDRESS + 4);
+            /* Jump to user application */
+            Jump_To_Application = (pFunction)JumpAddress;
+            /* Initialize user application's Stack Pointer */
+            __set_MSP(*(__IO uint32_t *)APPLICATION_ADDRESS);
+            Jump_To_Application();
+        }
+        else if ((key == 0x34) && (FlashProtection == 1))
+        {
+            /* Disable the write protection */
+            switch (FLASH_If_DisableWriteProtection())
+            {
+            case 1:
+            {
+                SerialPutString("Write Protection disabled...\r\n");
+                FlashProtection = 0;
+                break;
+            }
+            case 2:
+            {
+                SerialPutString("Error: Flash write unprotection failed...\r\n");
+                break;
+            }
+            default:
+            {
+            }
+            }
+        }
+        else
+        {
+            if (FlashProtection == 0)
+            {
+                SerialPutString("Invalid Number ! ==> The number should be either 1, 2 or 3\r");
+            }
+            else
+            {
+                SerialPutString("Invalid Number ! ==> The number should be either 1, 2, 3 or 4\r");
+            }
+        }
     }
-    else
-    {
-      if (FlashProtection == 0)
-      {
-        SerialPutString("Invalid Number ! ==> The number should be either 1, 2 or 3\r");
-      }
-      else
-      {
-        SerialPutString("Invalid Number ! ==> The number should be either 1, 2, 3 or 4\r");
-      }
-    }
-  }
 }
 
 /**
